245836181dea486eeafc38af19c809c548d892ce
cgnz0r <60849962+cgnz0r@users.noreply.github.com>
2022-05-07 21:34:31 +0700

    added tabindex and updated constants

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,13 +1,21 @@
+const CANVAS_SIZE       = 640
+const DIMENSION         = 16
+const GAP               = 1
+const CELL_SIZE         = CANVAS_SIZE / DIMENSION
+const RECT_CELL_SIZE    = CELL_SIZE - GAP * 2
+
+const PALETTE = {
+    secondaryColorsA: [ '#98ED00', '#82B22C', '#639A00', '#B4F63D', '#C6F66F' ],
+    secondaryColorsB: [ '#6C0AAB', '#5D2680', '#45036F', '#9A3FD5', '#AA67D5' ],
+    primaryColors   : [ '#FF9200', '#BF8230', '#A65F00', '#FFAD40', '#FFC373' ],
+    whiteColors     : [ '#FAFAFA' ]
+}
+
 export const settings = {
-    canvasWidth: 640,
-    cell: {
-        width: 40,
-        borderWidth: 1
-    },
-    palette: {
-        secondaryColorsA: [ '#98ED00', '#82B22C', '#639A00', '#B4F63D', '#C6F66F' ],
-        secondaryColorsB: [ '#6C0AAB', '#5D2680', '#45036F', '#9A3FD5', '#AA67D5' ],
-        primaryColors: [ '#FF9200', '#BF8230', '#A65F00', '#FFAD40', '#FFC373' ],
-        whiteColors: [ '#FAFAFA' ]
-    }
+    CANVAS_SIZE,
+    DIMENSION,
+    CELL_SIZE,
+    RECT_CELL_SIZE,
+    GAP,
+    PALETTE
 }
